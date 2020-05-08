a06d94b64364d7e3f974c5a992f8ee331c7a3b1c
paulinusia <paulina.adamski@gmail.com>
2020-05-08 00:12:57 -0500

    config template

---
+++ config/README.md
@@ -0,0 +1,15 @@
+All configurations go under this package.
+
+Configurations:
+
+kinesis_config:
+    REGION = ""
+    ACCESS_KEY = ""
+    SECRET = ""
+
+reddit_config:
+    CLIENT_ID = ""
+    SECRET = ""
+    USERNAME = ""
+    PASSWORD = ""
+    SECRET_AGENT = ""
--- data_stream_setup.py
+++ data_stream_setup.py
@@ -3,11 +3,7 @@ import os
 import json
 import boto3
 import praw 
-from config import kinesis_config, reddit_config
-# import config.kinesis_config as kinesis_config
-# import config.reddit_cred as reddit_cred
-
-
+from config.config import kinesis_config, reddit_config
 
 stream_name = 'reddit_news'
 
@@ -18,11 +14,11 @@ kinesis_client = boto3.client('kinesis',
                               aws_secret_access_key=kinesis_config.SECRET)
 
 # Initialize reddit stream
-reddit = praw.Reddit(client_id = reddit_cred.CLIENT_ID, 
-                   client_secret = reddit_cred.SECRET,
-                   username = reddit_cred.USERNAME,
-                   password = reddit_cred.PASSWORD,
-                   user_agent = reddit_cred.SECRET_AGENT)
+reddit = praw.Reddit(client_id = reddit_config.CLIENT_ID, 
+                   client_secret = reddit_config.SECRET,
+                   username = reddit_config.USERNAME,
+                   password = reddit_config.PASSWORD,
+                   user_agent = reddit_config.SECRET_AGENT)
 
 subreddits = reddit.subreddit("worldnews+politics+news+UpliftingNews")
 
@@ -32,14 +28,14 @@ def main():
                     'submission': str(submission.title),
                     'comment_number': int(submission.num_comments),
                     'score':  int(submission.score) }
-        
-        # RECORD
+
         try:
                 put_response = kinesis_client.put_record(
                                 StreamName=stream_name,
                                 Data=json.dumps(payload),
                                 PartitionKey= submission.name)
-                print('payload success')
+                print("Payload success:         {}".format(submission.name))
+
         except (AttributeError, Exception) as e:
             print (e)
 
